418a0993c4e7ace6933e2d5d2504989ff2fad285
unknown <samantha.c.egge@gmail.com>
2019-06-05 13:57:38 -0700

    Made an attempt at code for log-in page and function. It didn't break anything pre-implementation. Hold on to yer butts.

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -3,11 +3,41 @@ import React from 'react';
 import './Login.css';
 
 const Login = () => {
+    state = {
+        toggle: false
+    }
+
+    login = (event) => {
+        event.preventDefault();
+        if (localStorage.getItem('username')) {
+            localStorage.removeItem('username')
+            localStorage.setItem('username', JSON.stringify(event.target.value));
+            this.setState({
+                toggle: false
+            })
+        } else {
+            localStorage.setItem('username', JSON.stringify(event.target.value))
+            this.setState({
+                toggle: true
+            })
+        }
+    }
+
     return(
         <div className='login-container'>
-            <form className='login-form'>
-                <input></input>
+            <form className='login-form' onSubmit={login}>
+                <input
+                type='text'
+                placeholder='Username'
+                value='username' />
+                <input 
+                type='text'
+                placeholder='Password'
+                value='password' />
+                <button>Log-in</button>
             </form>
         </div>
     )
 }
+
+export default Login;
